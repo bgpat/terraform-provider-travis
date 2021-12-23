537a587a158a286049153525ad20307289f18439
bgpat <bgpat23@gmail.com>
2021-12-24 01:45:44 +0900

    Bump terraform sdk to v2

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	"github.com/bgpat/terraform-provider-travis/travis"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
-			return travis.Provider()
-		},
+		ProviderFunc: travis.Provider,
 	})
 }
